ce22961601918b8c396a03b8448fcae2b08cc339
ralfduerig <duerig@de.ibm.com>
2016-04-11 13:09:26 +0200

    Update deploycontainer.sh

--- deploycontainer.sh
+++ deploycontainer.sh
@@ -19,21 +19,21 @@ print_run_fail_msg () {
 log_and_echo ""
 log_and_echo "When a container cannot be created, the following are a common set of debugging steps."
 log_and_echo ""
-log_and_echo "1. Install Python, Pip, IBM Container Service CLI (ice), Cloud Foundry CLI, and Docker in your environment."
+log_and_echo "1. Install Python, Pip, IBM Container Servcf ic CLI (ice), Cloud Foundry CLI, and Docker in your environment."
 log_and_echo ""
 log_and_echo "2. Log into IBM Container Service."
-log_and_echo " ${green}ice login ${no_color}"
+log_and_echo " ${green}cf ic login ${no_color}"
 log_and_echo " or"
 log_and_echo " ${green}cf login ${no_color}"
 log_and_echo ""
-log_and_echo "2. Run 'ice run --verbose' in your current space or try it on another space. Check the output for information about the failure."
-log_and_echo " ${green}ice --verbose run --name ${MY_CONTAINER_NAME} ${PUBLISH_PORT} ${MEMORY} ${OPTIONAL_ARGS} ${BIND_PARMS} ${IMAGE_NAME} ${no_color}"
+log_and_echo "2. Run 'cf ic run --verbose' in your current space or try it on another space. Check the output for information about the failure."
+log_and_echo " ${green}cf ic --verbose run --name ${MY_CONTAINER_NAME} ${PUBLISH_PORT} ${MEMORY} ${OPTIONAL_ARGS} ${BIND_PARMS} ${IMAGE_NAME} ${no_color}"
 log_and_echo ""
 log_and_echo "3. Test the container locally."
 log_and_echo " a. Pull the image to your computer."
 log_and_echo " ${green}docker pull ${IMAGE_NAME} ${no_color}"
 log_and_echo " or"
-log_and_echo " ${green}ice --local pull ${IMAGE_NAME} ${no_color}"
+log_and_echo " ${green}cf ic --local pull ${IMAGE_NAME} ${no_color}"
 log_and_echo " b. Run the container locally by using the Docker run command and allow it to run for several minutes. Verify that the container continues to run. If the container stops, this will cause a crashed container on Bluemix."
 log_and_echo " ${green}docker run --name=mytestcontainer ${IMAGE_NAME} ${no_color}"
 log_and_echo " ${green}docker stop mytestcontainer ${no_color}"
@@ -41,8 +41,8 @@ log_and_echo " c. If you find an issue with the image locally, fix the issue, an
 log_and_echo " [fix and update your local Dockerfile]"
 log_and_echo " ${green}docker build -t ${IMAGE_NAME%:*}:test . ${no_color}"
 log_and_echo " ${green}docker push ${IMAGE_NAME%:*}:test ${no_color}"
-log_and_echo " d. Test the changes to the image on Bluemix using the 'ice run' command to determine if the container will now run on Bluemix."
-log_and_echo " ${green}ice --verbose run --name ${MY_CONTAINER_NAME}_test ${PUBLISH_PORT} ${MEMORY} ${OPTIONAL_ARGS} ${BIND_PARMS} ${IMAGE_NAME%:*}:test ${no_color}"
+log_and_echo " d. Test the changes to the image on Bluemix using the 'cf ic run' command to determine if the container will now run on Bluemix."
+log_and_echo " ${green}cf ic --verbose run --name ${MY_CONTAINER_NAME}_test ${PUBLISH_PORT} ${MEMORY} ${OPTIONAL_ARGS} ${BIND_PARMS} ${IMAGE_NAME%:*}:test ${no_color}"
 log_and_echo ""
 log_and_echo "4. Once the problem has been diagnosed and fixed, check in the changes to the Dockerfile and project into your IBM DevOps Services project and re-run this Pipeline."
 log_and_echo ""
@@ -53,7 +53,7 @@ dump_info () {
 log_and_echo "$LABEL" "Container Information: "
 log_and_echo "$LABEL" "Information about this organization and space:"
 log_and_echo "$INFO" " Summary:"
-local ICEINFO=$(ice info 2>/dev/null)
+local ICEINFO=$(cf ic info 2>/dev/null)
 log_and_echo "$INFO" " $ICEINFO"
 # check memory limit, warn user if we're at or approaching the limit
 export MEMORY_LIMIT=$(echo "$ICEINFO" | grep "Memory limit" | awk '{print $5}')
@@ -79,15 +79,15 @@ fi
 # echo -e "${label_color}You have ${AVAILABLE} public IP addresses remaining${no_color}"
 # fi
 log_and_echo "Groups: "
-log_and_echo `ice group list 2> /dev/null`
+log_and_echo `cf ic group list 2> /dev/null`
 log_and_echo "Routes: "
 log_and_echo `cf routes`
 log_and_echo "Running Containers: "
-log_and_echo `ice ps 2> /dev/null`
+log_and_echo `cf ic ps 2> /dev/null`
 log_and_echo "Floating IP addresses"
-log_and_echo `ice ip list 2> /dev/null`
+log_and_echo `cf ic ip list 2> /dev/null`
 log_and_echo "Images:"
-log_and_echo `ice images`
+log_and_echo `cf ic images`
 return 0
 }
 update_inventory(){
@@ -101,18 +101,18 @@ fi
 local ID="undefined"
 # find the container or group id
 if [ "$TYPE" == "ibm_containers" ]; then
-ID=$(ice inspect ${NAME} 2> /dev/null | grep "\"Id\":" | awk '{print $2}')
+ID=$(cf ic inspect ${NAME} 2> /dev/null | grep "\"Id\":" | awk '{print $2}')
 local RESULT=$?
 if [ $RESULT -ne 0 ]; then
 log_and_echo "$ERROR" "Could not find container called $NAME"
-ice ps 2> /dev/null
+cf ic ps 2> /dev/null
 return 1
 fi
 elif [ "${TYPE}" == "ibm_containers_group" ]; then
-ID=$(ice group inspect ${NAME} 2> /dev/null | grep "\"Id\":" | awk '{print $2}')
+ID=$(cf ic group inspect ${NAME} 2> /dev/null | grep "\"Id\":" | awk '{print $2}')
 if [ $RESULT -ne 0 ]; then
 log_and_echo "$ERROR" "Could not find group called $NAME"
-ice group list 2> /dev/null
+cf ic group list 2> /dev/null
 return 1
 fi
 else
@@ -153,7 +153,7 @@ IDS_RESOURCE=$CF_SPACE_ID
 if [ -z "$IDS_RESOURCE" ]; then
 log_and_echo "$ERROR" "Could not find CF SPACE in environment, using production space id"
 else
-# call IBM DevOps Service Inventory CLI to update the entry for this deployment
+# call IBM DevOps Servcf ic Inventory CLI to update the entry for this deployment
 log_and_echo "bash ids-inv -a ${ACTION} -d $IDS_DEPLOYER -q $IDS_REQUEST -r $IDS_RESOURCE -s $ID -t ${TYPE} -u $IDS_INV_URL -v $IDS_VERSION"
 bash ids-inv -a ${ACTION} -d $IDS_DEPLOYER -q $IDS_REQUEST -r $IDS_RESOURCE -s $ID -t ${TYPE} -u $IDS_INV_URL -v $IDS_VERSION
 fi
@@ -176,7 +176,7 @@ local COUNTER=0
 local STATE="unknown"
 while [[ ( $COUNTER -lt 180 ) && ("${STATE}" != "Running") && ("${STATE}" != "Crashed") ]]; do
 let COUNTER=COUNTER+1
-STATE=$(ice inspect $WAITING_FOR 2> /dev/null | grep "Status" | awk '{print $2}' | sed 's/"//g')
+STATE=$(cf ic inspect $WAITING_FOR 2> /dev/null | grep "Status" | awk '{print $2}' | sed 's/"//g')
 if [ -z "${STATE}" ]; then
 STATE="being placed"
 fi
@@ -204,7 +204,7 @@ local COUNTER=0
 local FOUND=0
 while [[ ( $COUNTER -lt 60 ) && ("${STATE}" != "Shutdown") ]]; do
 let COUNTER=COUNTER+1
-STATE=$(ice inspect $WAITING_FOR 2> /dev/null | grep "Status" | awk '{print $2}' | sed 's/"//g')
+STATE=$(cf ic inspect $WAITING_FOR 2> /dev/null | grep "Status" | awk '{print $2}' | sed 's/"//g')
 if [ -z "${STATE}" ]; then
 STATE="being deleted"
 fi
@@ -226,7 +226,7 @@ log_and_echo "$ERROR" "No container name was provided"
 return 1
 fi
 # check to see if that container name is already in use
-ice inspect ${MY_CONTAINER_NAME} > /dev/null
+cf ic inspect ${MY_CONTAINER_NAME} > /dev/null
 local FOUND=$?
 if [ ${FOUND} -eq 0 ]; then
 log_and_echo "$ERROR" "${MY_CONTAINER_NAME} already exists. Please remove these containers or change the Name of the container or group being deployed"
@@ -248,8 +248,8 @@ fi
 BIND_PARMS="--bind ${BIND_TO}"
 fi
 # run the container and check the results
-log_and_echo "run the container: ice run --name ${MY_CONTAINER_NAME} ${PUBLISH_PORT} ${MEMORY} ${OPTIONAL_ARGS} ${BIND_PARMS} ${IMAGE_NAME} "
-ice run --name ${MY_CONTAINER_NAME} ${PUBLISH_PORT} ${MEMORY} ${OPTIONAL_ARGS} ${BIND_PARMS} ${IMAGE_NAME} 2> /dev/null
+log_and_echo "run the container: cf ic run --name ${MY_CONTAINER_NAME} ${PUBLISH_PORT} ${MEMORY} ${OPTIONAL_ARGS} ${BIND_PARMS} ${IMAGE_NAME} "
+cf ic run --name ${MY_CONTAINER_NAME} ${PUBLISH_PORT} ${MEMORY} ${OPTIONAL_ARGS} ${BIND_PARMS} ${IMAGE_NAME} 2> /dev/null
 local RESULT=$?
 if [ $RESULT -ne 0 ]; then
 log_and_echo "$ERROR" "Failed to deploy ${MY_CONTAINER_NAME} using ${IMAGE_NAME}"
@@ -264,9 +264,9 @@ insert_inventory "ibm_containers" ${MY_CONTAINER_NAME}
 elif [ $RESULT -eq 2 ]; then
 log_and_echo "$ERROR" "Container instance crashed."
 log_and_echo "$WARN" "The container was removed successfully."
-ice rm ${MY_CONTAINER_NAME} 2> /dev/null
+cf ic rm ${MY_CONTAINER_NAME} 2> /dev/null
 if [ $? -ne 0 ]; then
-log_and_echo "$WARN" "'ice rm ${MY_CONTAINER_NAME}' command failed with return code ${RESULT}"
+log_and_echo "$WARN" "'cf ic rm ${MY_CONTAINER_NAME}' command failed with return code ${RESULT}"
 log_and_echo "$WARN" "Removing Container instance ${MY_CONTAINER_NAME} is not completed"
 fi
 print_run_fail_msg
@@ -307,16 +307,16 @@ if [ -n "${DISCOVERED_FLOATING_IP}" ]; then
 FLOATING_IP=$DISCOVERED_FLOATING_IP
 fi
 # check to see that I obtained a floating IP address
-#ice inspect ${CONTAINER_NAME}_${BUILD_NUMBER} > inspect.log
+#cf ic inspect ${CONTAINER_NAME}_${BUILD_NUMBER} > inspect.log
 #FLOATING_IP=$(cat inspect.log | grep "PublicIpAddress" | awk '{print $2}')
 if [ "${FLOATING_IP}" = '""' ] || [ -z "${FLOATING_IP}" ]; then
 log_and_echo "Requesting IP"
-FLOATING_IP=$(ice ip request 2> /dev/null | awk '{print $4}' | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+FLOATING_IP=$(cf ic ip request 2> /dev/null | awk '{print $4}' | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
 log_and_echo "$WARN" "Failed to request new IP address, will attempt to reuse existing IP"
-FLOATING_IP=$(ice ip list 2> /dev/null | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}[[:space:]]*$' | head -n 1)
-#FLOATING_IP=$(ice ip list 2> /dev/null | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -n 1)
+FLOATING_IP=$(cf ic ip list 2> /dev/null | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}[[:space:]]*$' | head -n 1)
+#FLOATING_IP=$(cf ic ip list 2> /dev/null | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -n 1)
 #strip off whitespace
 FLOATING_IP=${FLOATING_IP// /}
 if [ -z "${FLOATING_IP}" ];then
@@ -333,7 +333,7 @@ temp="${FLOATING_IP%\"}"
 FLOATING_IP="${temp#\"}"
 log_and_echo "Assigning new IP address $FLOATING_IP"
 fi
-ice ip bind ${FLOATING_IP} ${CONTAINER_NAME}_${BUILD_NUMBER} 2> /dev/null
+cf ic ip bind ${FLOATING_IP} ${CONTAINER_NAME}_${BUILD_NUMBER} 2> /dev/null
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
 log_and_echo "$ERROR" "Failed to bind ${FLOATING_IP} to ${CONTAINER_NAME}_${BUILD_NUMBER} "
@@ -361,10 +361,10 @@ do
 KEEP_BUILD_NUMBERS[$i]="${CONTAINER_NAME}_$(($BUILD_NUMBER-$i))"
 done
 # add the current containers in an array of the container name
-local CONTAINER_NAME_ARRAY=$(ice ps -q | grep ${CONTAINER_NAME} | awk '{print $2}')
+local CONTAINER_NAME_ARRAY=$(cf ic ps -q | grep ${CONTAINER_NAME} | awk '{print $2}')
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
-log_and_echo "$WARN" "'ice ps -q' command failed with return code ${RESULT}"
+log_and_echo "$WARN" "'cf ic ps -q' command failed with return code ${RESULT}"
 log_and_echo "$WARN" "Cleaning up previous deployments is not completed"
 return 0
 fi
@@ -373,7 +373,7 @@ for containerName in ${CONTAINER_NAME_ARRAY[@]}
 do
 CONTAINER_VERSION_NUMBER=$(echo $containerName | sed 's#.*_##g')
 if [ $CONTAINER_VERSION_NUMBER -le $BUILD_NUMBER ]; then
-ice inspect ${containerName} > inspect.log 2> /dev/null
+cf ic inspect ${containerName} > inspect.log 2> /dev/null
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
 log_and_echo "Found container ${containerName}"
@@ -396,18 +396,18 @@ if [ -z "${FLOATING_IP}" ]; then
 log_and_echo "${containerName} did not have a floating IP so will need to discover one from previous deployment or allocate one"
 elif [ -n "${IP_JUST_FOUND}" ]; then
 log_and_echo "${containerName} had a floating ip ${FLOATING_IP}"
-ice ip unbind ${FLOATING_IP} ${containerName} 2> /dev/null
+cf ic ip unbind ${FLOATING_IP} ${containerName} 2> /dev/null
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
-log_and_echo "$WARN" "'ice ip unbind ${FLOATING_IP} ${containerName}' command failed with return code ${RESULT}"
+log_and_echo "$WARN" "'cf ic ip unbind ${FLOATING_IP} ${containerName}' command failed with return code ${RESULT}"
 log_and_echo "$WARN" "Cleaning up previous deployments is not completed"
 return 0
 fi
 sleep 2
-ice ip bind ${FLOATING_IP} ${CONTAINER_NAME}_${BUILD_NUMBER} 2> /dev/null
+cf ic ip bind ${FLOATING_IP} ${CONTAINER_NAME}_${BUILD_NUMBER} 2> /dev/null
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
-log_and_echo "$WARN" "'ice ip bind ${FLOATING_IP} ${CONTAINER_NAME}_${BUILD_NUMBER}' command failed with return code ${RESULT}"
+log_and_echo "$WARN" "'cf ic ip bind ${FLOATING_IP} ${CONTAINER_NAME}_${BUILD_NUMBER}' command failed with return code ${RESULT}"
 log_and_echo "$WARN" "Cleaning up previous deployments is not completed"
 return 0
 fi
@@ -416,16 +416,16 @@ fi
 fi
 if [ $CONTAINER_VERSION_NUMBER -gt $BUILD_NUMBER ]; then
 log_and_echo "$WARN" "The container ${containerName} version is greater then the current build number ${BUILD_NUMBER} and it will not be removed."
-log_and_echo "$WARN" "You may remove it with the ice cli command 'ice rm -f ${containerName}'"
+log_and_echo "$WARN" "You may remove it with the cf ic cli command 'cf ic rm -f ${containerName}'"
 elif [[ " ${KEEP_BUILD_NUMBERS[*]} " == *" ${containerName} "* ]]; then
 # this is the concurrent version so keep it around
 log_and_echo "keeping deployment: ${containerName}"
 else
 log_and_echo "removing previous deployment: ${containerName}"
-ice rm -f ${containerName} 2> /dev/null
+cf ic rm -f ${containerName} 2> /dev/null
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
-log_and_echo "$WARN" "'ice rm -f ${containerName}' command failed with return code ${RESULT}"
+log_and_echo "$WARN" "'cf ic rm -f ${containerName}' command failed with return code ${RESULT}"
 log_and_echo "$WARN" "Cleaning up previous deployments is not completed"
 return 0
 fi
